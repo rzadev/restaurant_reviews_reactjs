70ced3f54318142d8e4d37352fc295c4a709dc1d
creative301 <rza9001@gmail.com>
2019-10-06 07:17:50 +0700

    Add the API key

--- src/api/googlemaps.js
+++ src/api/googlemaps.js
@@ -10,7 +10,7 @@ export function load_google_maps() {
     };
     // Now, Load the Google Maps API
     const script = document.createElement('script');
-    const API_KEY = '';
+    const API_KEY = 'AIzaSyBdfaGvQ20-Yvwx7FUtPqfvC2jrDHDGlxY';
     script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
     script.async = true;
     document.body.appendChild(script);
@@ -23,6 +23,6 @@ export function getGoogleImage(restaurantsMarkers) {
     restaurantsMarkers.lat +
     ',' +
     restaurantsMarkers.lng +
-    '&heading=151.78&pitch=-0.76&key='
+    '&heading=151.78&pitch=-0.76&key=AIzaSyBdfaGvQ20-Yvwx7FUtPqfvC2jrDHDGlxY'
   );
 }
